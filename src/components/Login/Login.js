722de1069c75d15a628dcbe1d8d673833ebacdc8
creativerze <creativerze@gmail.com>
2022-12-28 01:48:29 +0600

    added email password authentication

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,16 +1,32 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { useForm } from 'react-hook-form';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import img from '../../assets/login.jpg';
+import { AuthContext } from '../../contexts/AuthProvider';
 
 const Login = () => {
-
   const { register, formState: { errors }, handleSubmit } = useForm();
+  const { signIn } = useContext(AuthContext);
   const [loginError, setLoginError] = useState('');
+
+  const navigate = useNavigate();
 
   const handleLogin = data => {
     console.log(data);
     setLoginError('');
+
+    signIn(data.email, data.password)
+      .then(result => {
+        const user = result.user;
+        console.log(user);
+        // navigate(from, { replace: true });
+        navigate('/');
+      })
+      .catch(error => {
+        console.log(error.message);
+        setLoginError(error.message);
+      });
+
   };
 
   return (
@@ -24,22 +40,22 @@ const Login = () => {
 
           <form onSubmit={handleSubmit(handleLogin)}>
 
-            <div className="form-control w-full max-w-xs">
+            <div className="form-control w-full">
               <label className="label">
                 <span className="label-text-alt">Email</span>
               </label>
-              <input {...register("email", { required: 'Email is required' })} type="email" className="input input-bordered w-full max-w-xs" />
+              <input {...register("email", { required: 'Email is required' })} type="email" className="input input-bordered w-full" />
               {errors?.email && <p className='text-red-600'>{errors.email?.message}</p>}
             </div>
 
-            <div className="form-control w-full max-w-xs">
+            <div className="form-control w-full">
               <label className="label">
                 <span className="label-text-alt">Password</span>
               </label>
               <input {...register("password", {
                 required: 'Password is required',
                 minLength: { value: 6, message: 'Password must be 6 characters or longer' }
-              })} type="password" className="input input-bordered w-full max-w-xs" />
+              })} type="password" className="input input-bordered w-full" />
               {errors?.password && <p className='text-red-600'>{errors.password?.message}</p>}
               <label className="label">
                 <span className="label-text-alt">Forget Password?</span>
---
+++ src/components/SignUp/SignUp.js
@@ -0,0 +1,83 @@
+import React, { useContext, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
+import img from '../../assets/login.jpg';
+import { AuthContext } from '../../contexts/AuthProvider';
+
+const SignUp = () => {
+
+  const { register, handleSubmit, formState: { errors } } = useForm();
+
+  const { createUser, googleSignIn } = useContext(AuthContext);
+
+  const [signUpError, setSignUPError] = useState('');
+
+  const handleSignUp = (data) => {
+    console.log('data', data);
+
+    setSignUPError('');
+
+    createUser(data.email, data.password)
+      .then(result => {
+        const user = result.user;
+        console.log('user', user);
+        // toast('User Created Successfully.');
+      })
+      .catch(error => {
+        console.log(error);
+        setSignUPError(error.message);
+      });
+
+
+  };
+
+  return (
+    <div className="hero w-full my-10">
+      <div className="hero-content gap-10 grid md:grid-cols-2 flex-col lg:flex-row">
+        <div className="text-center lg:text-left">
+          <img className='w-3/4 mx-auto rounded-lg' src={img} alt="" />
+        </div>
+        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 py-10">
+          <h1 className="text-5xl text-center font-bold text-indigo-600">Sign Up</h1>
+
+          <form onSubmit={handleSubmit(handleSignUp)}>
+
+            <div className="form-control w-full max-w-xs">
+              <label className="label"> <span className="label-text">Name</span></label>
+              <input type="text" {...register("name", {
+                required: "Name is Required"
+              })} className="input input-bordered w-full max-w-xs" />
+              {errors.name && <p className='text-red-500'>{errors.name.message}</p>}
+            </div>
+
+            <div className="form-control w-full max-w-xs">
+              <label className="label"> <span className="label-text">Email</span></label>
+              <input type="email" {...register("email", {
+                required: true
+              })} className="input input-bordered w-full max-w-xs" />
+              {errors.email && <p className='text-red-500'>{errors.email.message}</p>}
+            </div>
+
+            <div className="form-control w-full max-w-xs">
+              <label className="label"> <span className="label-text">Password</span></label>
+              <input type="password" {...register("password", {
+                required: "Password is required",
+                minLength: { value: 6, message: "Password must be 6 characters long" },
+                pattern: { value: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])/, message: 'Password must have uppercase, number and special characters' }
+              })} className="input input-bordered w-full max-w-xs" />
+              {errors.password && <p className='text-red-500'>{errors.password.message}</p>}
+            </div>
+
+            <input className='btn btn-primary w-full mt-4' value="Sign Up" type="submit" />
+            {signUpError && <p className='text-red-600'>{signUpError}</p>}
+          </form>
+          <p>Already have an account <Link className='text-primary' to="/login">Please Login</Link></p>
+
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
